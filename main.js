11a4484a725a9fcedd766e79e3753a5073cbe581
jjdavenport <jordanjamesdavenport@gmail.com>
2024-04-19 21:25:41 +0100

    completed all functions

--- main.js
+++ main.js
@@ -4,27 +4,6 @@ const hidden = document.querySelector(".hidden");
 const backButton = document.getElementById("back-btn");
 const btnPath = document.getElementById("btn-path");
 
-btnPath.addEventListener("mouseover", () => {
-  btnPath.classList.add("path-fill");
-});
-
-btnPath.addEventListener("mousedown", () => {
-  btnPath.classList.remove("path-fill");
-});
-
-button.addEventListener("click", () => {
-  const width = window.innerWidth;
-  if (width < 560) {
-    hidden.classList.toggle("hidden-active");
-    row.classList.toggle("row-hide");
-  } else {
-    button.classList.toggle("mobile");
-    button.classList.toggle("desktop");
-    hidden.classList.toggle("hidden-active");
-    btnPath.classList.toggle("desktop");
-  }
-});
-
 backButton.addEventListener("click", () => {
   const width = window.innerWidth;
   if (width < 560) {
@@ -33,4 +12,26 @@ backButton.addEventListener("click", () => {
   } else {
     hidden.classList.toggle("hidden-active");
   }
+});
+
+button.addEventListener("click", () => {
+  const width = window.innerWidth;
+  buttonClicked = true;
+  if (width < 560) {
+    hidden.classList.toggle("hidden-active");
+    row.classList.toggle("row-hide");
+  } else {
+    button.classList.toggle("mobile");
+    button.classList.toggle("desktop");
+    hidden.classList.toggle("hidden-active");
+    btnPath.classList.toggle("path-desktop");
+  }
+});
+
+button.addEventListener("mouseover", () => {
+  btnPath.setAttribute("fill", "#fff");
+});
+
+button.addEventListener("mouseout", () => {
+  btnPath.setAttribute("fill", "#6E8098");
 });
