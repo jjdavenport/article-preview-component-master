d5e69f67efa4a508da8c6b581eb33096d2b5c61e
jjdavenport <jordanjamesdavenport@gmail.com>
2024-04-19 16:16:30 +0100

    completed click and mouseover functions

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ const button = document.getElementById("btn");
 const row = document.querySelector(".row");
 const hidden = document.querySelector(".hidden");
 const backButton = document.getElementById("back-btn");
+const btnPath = document.getElementById("btn-path");
+
+btnPath.addEventListener("mouseover", () => {
+  btnPath.classList.add("path-fill");
+});
+
+btnPath.addEventListener("mousedown", () => {
+  btnPath.classList.remove("path-fill");
+});
 
 button.addEventListener("click", () => {
   const width = window.innerWidth;
@@ -9,7 +18,10 @@ button.addEventListener("click", () => {
     hidden.classList.toggle("hidden-active");
     row.classList.toggle("row-hide");
   } else {
+    button.classList.toggle("mobile");
+    button.classList.toggle("desktop");
     hidden.classList.toggle("hidden-active");
+    btnPath.classList.toggle("desktop");
   }
 });
 
