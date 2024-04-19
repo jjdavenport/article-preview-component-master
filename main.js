1c652cb35222cd7d89421f3fe76b96b1c71e55f3
jjdavenport <jordanjamesdavenport@gmail.com>
2024-04-19 13:30:25 +0100

    updated function with toggle

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const hidden = document.querySelector(".hidden");
 const backButton = document.getElementById("back-btn");
 
 button.addEventListener("click", () => {
-  row.style = "display: none";
-  hidden.style = "display: flex";
+  hidden.classList.toggle("hidden-active");
+  row.classList.toggle("row-hide");
 });
 
 backButton.addEventListener("click", () => {
-  hidden.style = "display: none";
-  row.style = "display: flex";
+  row.classList.toggle("row-hide");
+  hidden.classList.toggle("hidden-active");
 });
